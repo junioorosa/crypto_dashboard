ddf7d423dc791ed85072598290f652c471379d58
junioorosa <86578051+junioorosa@users.noreply.github.com>
2023-02-09 22:47:38 -0300

    criado entidade usuário

--- src/main/java/br/com/crypto_dashboard/CryptoDashboardApplication.java
+++ src/main/java/br/com/crypto_dashboard/CryptoDashboardApplication.java
@@ -1,4 +1,4 @@
-package br.com.crypto;
+package br.com.crypto_dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/crypto_dashboard/CryptoDashboardApplicationTests.java
+++ src/test/java/br/com/crypto_dashboard/CryptoDashboardApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.crypto;
+package br.com.crypto_dashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
